73c869b5bb1f8a90f8a70c9e35ae0a6635d1604c
Steven <stevenp0294@gmail.com>
2022-09-16 13:31:41 -0400

    Created a gameboard within an array within a gamboard object
    I anticipate this being quite the learning curve for me.
    Introduction in factory functions and modules has been rocky.

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log('Hello World!')
+//STEP ONE:
+//Store the gameboard in an array inside of a Gameboard object.
+//try and make the game functional without html and css
+
+//STEP TWO:
+//Players are going to be stored in objects as well
+
+function makeBoard(){
+  const board_div = document.getElementById('game-grid');
+  for(let i = 0; i < 3; i++){
+    let row = document.createElement('div');
+    for(let i = s0; i < 3; i++){
+      let cell = doucment.createElement('div');
+      row.appendChild(cell);
+    }
+  };
+};
+
+let makeBoard = (function(){
+  //code here:
+})();
+
+const Gameboard = (function() {
+  const boardArr = [
+  ' ', ' ', ' ',
+  ' ', ' ', ' ',
+  ' ', ' ', ' ',
+  ];
+  return {
+    showArray: (() => {
+      boardArr.forEach((spot) => {
+        if(spot = ' '){
+          spot = 'It works!';
+        };
+        console.log(spot);
+      });
+    })
+  };
+})();
+
+Gameboard.showArray();
