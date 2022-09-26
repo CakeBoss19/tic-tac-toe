71e6b62b97390ad41ed8e82281511cbecf3942bb
Steven <stevenp0294@gmail.com>
2022-09-26 12:09:06 -0400

    Created playerCreator factory functions and main game event listeners Gameboard is able to be altered, but no 'win' conditions have been set yet.
    code will still need to be consolidated into more organized methods and functions.

--- script.js
+++ script.js
@@ -1,33 +1,67 @@
-const X_CLASS = 'x';
-const CIRCLE_CLASS = 'circle';
-const cellElements = document.querySelectorAll('[data-cell]');
-const board = document.getElementById('board');
-let circleTurn = false;
+// const X_CLASS = 'x';
+// const CIRCLE_CLASS = 'circle';
+// const cellElements = document.querySelectorAll('[data-cell]');
+// let circleTurn;
 
-cellElements.forEach(cell => {
-  cell.addEventListener('click', handleClick, { once: true })
+
+// cellElements.forEach(cell => {
+//   cell.addEventListener('click', handleClick, { once: true })
+// });
+// function handleClick(e){
+//   const cell = e.target;
+//   const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+//   placeMark(cell, currentClass);
+//   swapTurns();
+// };
+
+// function placeMark(cell, currentClass){
+//   cell.classList.add(currentClass)
+// };
+
+// function swapTurns(currentClass){
+//   circleTurn = !circleTurn;
+//   board.classList.add(currentClass);
+// };
+
+
+let playerCreator = ((index) => {
+
+  //if player1 is currently player,
+  //I'm going to want to change zero of the selected spot to a 1;
+  function makeMove(cell){
+    let playerSelection = cell.dataset.cell;
+    gameboard.splice(playerSelection, 1, index);
+    console.log(gameboard);
+  };
+  return {makeMove};
 });
 
-function handleClick(e){
-  const cell = e.target;
-  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
-  placeMark(cell, currentClass);
-  swapTurns();
-  swapBoard();
-  // Check for win
-  // Check for draw
-};
 
-function placeMark(cell, currentClass){
-  cell.classList.add(currentClass);
-};
+const cells = document.querySelectorAll(".cell");
+const playerOne = playerCreator(1);
+const playerTwo = playerCreator(2);
 
-function swapTurns(){
-  circleTurn = !circleTurn;
-};
+let gameboard = [
+  0, 0, 0,
+  0, 0, 0,
+  0, 0, 0,
+];
 
-function swapBoard(){
-  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
-  board.classList = '';
-  board.classList = `board ${currentClass}`;  
+let n = 0;
+
+cells.forEach((cell) => {
+  
+  cell.addEventListener('click', (e) => {
+    let currentPlayer = getCurrentPlayer();
+    currentPlayer.makeMove(e.target);
+
+    // examineBoard(); // checks array to see if any victory conditions have been met.
+    n++;
+  });
+});
+
+
+
+function getCurrentPlayer(){
+  return (n % 2 == 0 ? playerOne : playerTwo)
 }
