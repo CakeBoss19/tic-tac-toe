e6689a674b2962ec875c16c971e095efbd40421c
Steven <stevenp0294@gmail.com>
2022-09-26 20:51:49 -0400

    finished the game, except for checking for win status' and exiting game. I plan on displaying a win message, displaying who wins and a button to reset the game. checkForWin() could be organized a bit better, but serves it's purpose

--- script.js
+++ script.js
@@ -14,11 +14,11 @@ const cssChanges = (() => {
 })();
 
 const playerCreator = ((player) => {
-
+  let playerValue = player + 1;
   function makeMove(e){
     const cell = e.target;
     const cellIndex = cell.dataset.cell;
-    gameboard.splice(cellIndex, 1, (player + 1));
+    gameboard.splice(cellIndex, 1, playerValue);
   };
 
   function changeDisplay(e){
@@ -30,40 +30,54 @@ const playerCreator = ((player) => {
     cssChanges.placeMark(cell, currentClass);
     cssChanges.boardClasses();
   }
-  return {makeMove, changeDisplay};
+
+  function checkForWin(){
+    if(gameboard[0] == playerValue && gameboard[1] == playerValue && gameboard[2] == playerValue){
+      console.log('winner');
+    } else if(gameboard[0] == playerValue && gameboard[3] == playerValue && gameboard[6] == playerValue){
+      console.log('you did it!');
+    } else if(gameboard[0] == playerValue && gameboard[4] == playerValue && gameboard[8] == playerValue){
+      console.log('you did it!');
+    } else if(gameboard[1] == playerValue && gameboard[4] == playerValue && gameboard[7] == playerValue){
+      console.log('you did it!');
+    } else if(gameboard[2] == playerValue && gameboard[5] == playerValue && gameboard[8] == playerValue){
+      console.log('you did it!');
+    } else if(gameboard[2] == playerValue && gameboard[4] == playerValue && gameboard[6] == playerValue){
+      console.log('you did it!');
+    } else if(gameboard[3] == playerValue && gameboard[4] == playerValue && gameboard[5] == playerValue){
+      console.log('you did it!');
+    } else if(gameboard[6] == playerValue && gameboard[7] == playerValue && gameboard[8] == playerValue){
+      console.log('you did it!');
+    } else{
+      console.log('ohwell')
+    }
+  };
+
+  return {makeMove, changeDisplay, checkForWin};
 });
 
-const cellElements = document.querySelectorAll(".cell");
-const playerOne = playerCreator(0);
-const playerTwo = playerCreator(1);
+  const cellElements = document.querySelectorAll(".cell");
+  const playerOne = playerCreator(0);
+  const playerTwo = playerCreator(1);
 
-let gameboard = [
-  0, 0, 0,
-  0, 0, 0,
-  0, 0, 0,
-];
+  let gameboard = [
+    0, 0, 0,
+    0, 0, 0,
+    0, 0, 0,
+  ];
 
-let n = 0;
+  let n = 0;
 
-cellElements.forEach((cell) => {
-  
-  cell.addEventListener('click', (e) => {
-    let currentPlayer = getCurrentPlayer();
-    currentPlayer.makeMove(e);
-    currentPlayer.changeDisplay(e);
-    // examineBoard(); // checks array to see if any victory conditions have been met.
-    n++;
-  }, {once: true});
-});
-
-
+  cellElements.forEach((cell) => {
+    cell.addEventListener('click', (e) => {
+      let currentPlayer = getCurrentPlayer();
+      currentPlayer.makeMove(e);
+      currentPlayer.changeDisplay(e);
+      currentPlayer.checkForWin(); // checks array to see if any victory conditions have been met.
+      n++;
+    }, {once: true});
+  });
 
 function getCurrentPlayer(){
   return (n % 2 == 0 ? playerOne : playerTwo);
-}
-
-function examineBoard(){
-  //use a switch statment to determine winning states,
-  //if gameboard[0-2] =1 or 2, win, restart game
-  //different statement for winning condistions. reorganize later
 }
